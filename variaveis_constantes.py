1d17dcd93b0b5fbf94260150c2b38fdba60a720e
jotap53 <martinsjp53@gmail.com>
2026-04-14 09:01:01 +0000

    subindo arquivos

---
+++ variaveis_constantes.py
@@ -0,0 +1,15 @@
+nome = 'João'
+idade = 28
+
+nome, idade = 'Pedro', 29
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ['SP', 'RJ', 'CE']
+
+# Por mais que isso seja uma constante, se mudar o valor o interpretador não vai barrar.
+# Basicamente, ele ser colocado em caixa alta vai ser mais um direcionamento pro programador
+# do para o interpretador de fato.
+BRAZILIAN_STAES = 10
