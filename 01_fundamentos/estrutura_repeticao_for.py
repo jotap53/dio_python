e72b979f16f2385f4e9eafa127b4c3c5abf62b5f
jotap53 <martinsjp53@gmail.com>
2026-04-15 06:03:36 -0300

    subindo arquivos direto na main

---
+++ 01_fundamentos/estrutura_repeticao_for.py
@@ -0,0 +1,13 @@
+texto = input('Informe um texto: ')
+VOGAIS = 'AEIOU'
+
+# Exemplo utilizando um iterável
+for letra in texto:
+    if letra.upper() in VOGAIS:
+        print(letra, end='')
+
+print() # adiciona uma quebra de linha
+
+# Exemplo utilizando a função buuilt-in range
+for numero in range(0, 51, 5):
+    print(numero, end='')
