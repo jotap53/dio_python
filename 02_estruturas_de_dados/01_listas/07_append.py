04cc83863ed874a5a05254fb60869a2578a011c9
jotap53 <martinsjp53@gmail.com>
2026-04-21 10:50:41 -0300

    subindo arquivos

--- 02_estruturas_de_dados/01_listas/07_append.py
+++ 02_estruturas_de_dados/01_listas/07_append.py
@@ -3,5 +3,6 @@ lista = []
 lista.append(1)
 lista.append("Python")
 lista.append([40, 30, 20])
+lista.append(['João Pedro', '28 anos', 'Programador Backend'])
 
 print(lista)  # [1, "Python", [40, 30, 20]]
--- 02_estruturas_de_dados/02_tuplas/00_declarando_tuplas.py
+++ 02_estruturas_de_dados/02_tuplas/00_declarando_tuplas.py
@@ -13,3 +13,6 @@ print(numeros)
 
 pais = ("Brasil",)
 print(pais)
+
+carros = ("gol")
+print(isinstance(carros, tuple))
