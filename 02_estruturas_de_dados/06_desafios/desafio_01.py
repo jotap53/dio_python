74d8657b21ba0e67650c9bc0125891f807062d1c
Joao Pedro <martinsjp53@gmail.com>
2026-04-22 17:57:57 -0300

    subindo dois desafios

---
+++ 02_estruturas_de_dados/06_desafios/desafio_01.py
@@ -0,0 +1,42 @@
+"""
+Descrição
+Desenvolva uma função em Python que recebe uma tupla de números inteiros e retorna a soma de todos os elementos dessa tupla. A função deve ser capaz de lidar com tuplas de qualquer tamanho, contanto que todos os elementos sejam números inteiros. O objetivo é praticar a manipulação de tuplas e a utilização de funções em Python.
+
+Explicação de Resolução:
+
+1. Converta cada string na "lista_strings" em um número inteiro utilizando a função "int()".
+2. Use a função "map()" para aplicar a função "int()" a cada elemento da "lista_strings".
+3. Armazene o resultado em uma variável chamada "elementos".
+
+Documentação Oficial:
+https://docs.python.org/pt-br/3/library/stdtypes.html#tuple
+
+Entrada
+A entrada para o seu programa será uma única linha de texto contendo vários números inteiros separados por espaços. Esses números devem ser lidos e convertidos para uma tupla de inteiros.
+
+Saída
+A saída do seu programa deve ser a soma de todos os números inteiros presentes na tupla. O resultado deve ser exibido de forma clara e amigável, seguindo o formato especificado. Aqui estão os passos detalhados para a saída:
+
+1. Calcule a soma de todos os elementos da tupla.
+
+2. Exiba a soma do valor calculado no formato: A soma dos elementos da tupla é: <soma>
+
+Exemplos
+A tabela abaixo apresenta exemplos com alguns dados de entrada e suas respectivas saídas esperadas. Certifique-se de testar seu programa com esses exemplos e com outros casos possíveis.
+
+Entrada     |       Saída
+2 5 6 7 9   |       A soma dos elementos da tupla é: 29
+9 8 7 6 5   |       A soma dos elementos da tupla é: 35
+50 50 50 50 |       A soma dos elementos da tupla é: 200
+
+ATENÇÃO
+As entradas e saídas dos desafios de código devem ser idênticas às definidas na descrição do desafio.
+"""
+
+def soma_tupla():
+    lista_strings = input('').split()
+    elementos = list(map(int, lista_strings))
+    resultado = sum(elementos)
+    print(f'A soma dos elementos da tupla é: {resultado}')
+
+soma_tupla()
